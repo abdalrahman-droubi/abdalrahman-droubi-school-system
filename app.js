ddd925767d60e3b422d39040b299505c52ee2f8a
abdalrahman-droubi <abdalrahman.drobi@outlook.com>
2023-03-27 02:45:36 +0300

    constructor

--- app.js
+++ app.js
@@ -7,38 +7,71 @@ menuHamburger.addEventListener('click', () => {
 
 
 let form = document.getElementById("form");
-let table = document.getElementById("resultTable");
+let card = document.getElementById("card");
+let arrStuData = [];
+if (localStorage.stuData != null) {
+    arrStuData = JSON.parse(localStorage.stuData);
+    render();
+}
 
-form.addEventListener("submit", submitHandler);
 
-function submitHandler(e) {
-    e.preventDefault();
-    let row = document.createElement("tr");
+form.addEventListener("submit", function (e) {
+    let fname = e.target.fname.value;
+    let birth = e.target.birth.value;
+    let gender = e.target.gender.value;
+    let major = e.target.major.value;
+    let number = e.target.number.value;
+    let img = e.target.img.value;
+    console.log(fname, birth, gender, major, number, img);
 
-    let num = document.createElement("td");
-    num.textContent = "*"
-    row.appendChild(num);
+    function Student(fname, birth, gender, major, number, img) {
+        this.fname = fname;
+        this.birth = birth;
+        this.gender = gender;
+        this.major = major;
+        this.number = number;
+        this.img = img;
+    }
+    let studentObject = new Student(fname, birth, gender, major, number, img);
+    arrStuData.push(studentObject)
+    localStorage.setItem("stuData", JSON.stringify(arrStuData))
 
-    let name = document.createElement("td");
-    name.textContent = e.target.fname.value;
-    row.appendChild(name);
-
-    let date = document.createElement("td");
-    date.textContent = e.target.birth.value;
-    row.appendChild(date);
-
-    let gender = document.createElement("td");
-    gender.textContent = e.target.gender.value;
-    row.appendChild(gender);
-
-    let number = document.createElement("td");
-    number.textContent = e.target.number.value;
-    row.appendChild(number);
-
-    let grade = document.createElement("td");
-    grade.textContent = e.target.grade.value;
-    row.appendChild(grade);
-
-    table.appendChild(row);
+    render();
     form.reset();
+    console.log(arrStuData);
+})
+
+
+function render() {
+    for (let i = 0; i < arrStuData.length; i++) {
+
+        let imgStu = document.createElement("img")
+        imgStu.src = `${arrStuData[i].img}`
+        card.appendChild(imgStu)
+
+        let div = document.createElement("div");
+        div.className = "container"
+
+        let h3 = document.createElement("h3");
+        h3.textContent = `${arrStuData[i].fname}`;
+        div.appendChild(h3);
+
+        let pbirth = document.createElement("p");
+        pbirth.textContent = `${arrStuData[i].birth}`;
+        div.appendChild(pbirth);
+
+        let pgender = document.createElement("p");
+        pgender.textContent = `${arrStuData[i].gender}`;
+        div.appendChild(pgender);
+
+        let pmajor = document.createElement("p");
+        pmajor.textContent = `${arrStuData[i].major}`;
+        div.appendChild(pmajor);
+
+        let pnumber = document.createElement("p");
+        pnumber.textContent = `${arrStuData[i].number}`;
+        div.appendChild(pnumber);
+
+        card.appendChild(div);
+    }
 }
--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -1,67 +1,67 @@
-// 5- Write a function that, takes an array of integers as input, iterates over the array, and returns a new array.
-//  The returned array should contain the result of raising 2 to the power of the original input element.
-// For example,([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and 2 ^ 3 = 8.
-// Use for and forEach and then map to solve this question to see the difference between the three ways 
+// // 5- Write a function that, takes an array of integers as input, iterates over the array, and returns a new array.
+// //  The returned array should contain the result of raising 2 to the power of the original input element.
+// // For example,([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and 2 ^ 3 = 8.
+// // Use for and forEach and then map to solve this question to see the difference between the three ways 
 
-let arr1 = [1, 2, 3]
-for (let i = 0; i < arr1.length; i++) {
-    arr1[i] = 2 ** arr1[i]
+// let arr1 = [1, 2, 3]
+// for (let i = 0; i < arr1.length; i++) {
+//     arr1[i] = 2 ** arr1[i]
 
-}
-console.log(arr1);
-let arrForMap = [1, 2, 3]
+// }
+// console.log(arr1);
+// let arrForMap = [1, 2, 3]
 
-let arr2 = arrForMap.map(value => { return 2 ** value })
-console.log(arr2);
-
-
-arrForMap.forEach((value, index, arrForMap) => {
-    arrForMap[index] = 2 ** value
-})
-console.log(arrForMap);
+// let arr2 = arrForMap.map(value => { return 2 ** value })
+// console.log(arr2);
 
 
-// 6- Write a function that, takes an array of numbers as input, uses map to return a new array
-//  where each element is either the string "even" or the string "odd", based on each value.
-// If any element in the array is not a number, the resulting array should have the string "N/A" in its place.
-// For example:([1,2,3,"israa"]) returns ['odd','even','odd','N/A'].
+// arrForMap.forEach((value, index, arrForMap) => {
+//     arrForMap[index] = 2 ** value
+// })
+// console.log(arrForMap);
 
-let arrOddEven = [1, 2, 3, "abd"]
-let newArr = arrOddEven.map(value => {
-    if (isNaN(value)) {
-        return "N/A"
-    } else if (value % 2 == 0) {
-        return "Even"
-    }
-    else if (value % 2 != 0) {
-        return "Odd"
-    }
-})
-console.log(newArr);
 
-// 7- Write a function named fizzbuzz that takes in an array of numbers.
-// Iterate over the array using forEach or map to determine the output based on several rules:
-//   - If a number is divisible by 3, add the word "Fizz" to the output array.
-//   - If the number is divisible by 5, add the word "Buzz" to the output array.
-//   - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz" to the output array.
-//   - Otherwise, add the number to the output array.
-// Return the resulting output array.
+// // 6- Write a function that, takes an array of numbers as input, uses map to return a new array
+// //  where each element is either the string "even" or the string "odd", based on each value.
+// // If any element in the array is not a number, the resulting array should have the string "N/A" in its place.
+// // For example:([1,2,3,"israa"]) returns ['odd','even','odd','N/A'].
 
-let arrayFizzBuzz = [1, 3, 5, 15, 9, 14]
-function fizzbuzz(arr) {
-    let newArr = arr.map(value => {
-        if (value % 3 == 0 && value % 5 == 0) {
-            return "Fizz Buzz"
-        } else if (value % 3 == 0) {
-            return "Fizz"
-        } else if (value % 5 == 0) {
-            return "Buzz"
-        } else {
-            return value
-        }
-    })
-    console.log(newArr);
-}
+// let arrOddEven = [1, 2, 3, "abd"]
+// let newArr = arrOddEven.map(value => {
+//     if (isNaN(value)) {
+//         return "N/A"
+//     } else if (value % 2 == 0) {
+//         return "Even"
+//     }
+//     else if (value % 2 != 0) {
+//         return "Odd"
+//     }
+// })
+// console.log(newArr);
 
-fizzbuzz(arrayFizzBuzz)
+// // 7- Write a function named fizzbuzz that takes in an array of numbers.
+// // Iterate over the array using forEach or map to determine the output based on several rules:
+// //   - If a number is divisible by 3, add the word "Fizz" to the output array.
+// //   - If the number is divisible by 5, add the word "Buzz" to the output array.
+// //   - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz" to the output array.
+// //   - Otherwise, add the number to the output array.
+// // Return the resulting output array.
+
+// let arrayFizzBuzz = [1, 3, 5, 15, 9, 14]
+// function fizzbuzz(arr) {
+//     let newArr = arr.map(value => {
+//         if (value % 3 == 0 && value % 5 == 0) {
+//             return "Fizz Buzz"
+//         } else if (value % 3 == 0) {
+//             return "Fizz"
+//         } else if (value % 5 == 0) {
+//             return "Buzz"
+//         } else {
+//             return value
+//         }
+//     })
+//     console.log(newArr);
+// }
+
+// fizzbuzz(arrayFizzBuzz)
 
